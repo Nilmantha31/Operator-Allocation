6938da81983900334687164b6022071f070f9352
Nilmantha Vishwanath <VNilmantha@jayjaymills.lk>
2025-01-22 15:06:52 +0530

    added config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from .worker import celery_app
+from app.config import DevelopmentConfig
 from flask import Flask
 from .blueprints.opAutoAllocation_bp import opAutoAllocationBp
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
 CORS(app)
 
 flask_app = app
